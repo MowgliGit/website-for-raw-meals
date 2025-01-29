cc9c1f0fa11663a42f878df3d24b51508da44cd2
MowgliGit <marina.jovanova17@gmail.com>
2025-01-29 20:16:25 +0100

    some changes

--- js/script.js
+++ js/script.js
@@ -3,118 +3,100 @@
 // ********** //
 const randomColorNav = () => `rgb(84, 38, 38)`;
 
-
-document.querySelector('.header').addEventListener('click', function(e){
-    e.preventDefault;
-    this.style.backgroundColor = randomColorNav();
-})
+document.querySelector(".header").addEventListener("click", function (e) {
+  e.preventDefault;
+  this.style.backgroundColor = randomColorNav();
+});
 // ********** //
 //    MENU
 // ********** //
-const firstSlide = document.querySelector('.first-slide');
-const secondSlide = document.querySelector('.second-slide');
-const btnFill = document.querySelector('.btn-dot');
+const firstSlide = document.querySelector(".first-slide");
+const secondSlide = document.querySelector(".second-slide");
+const btnFill = document.querySelector(".btn-dot");
 
-document.querySelectorAll('.btn-dot').forEach(function (el){
-    el.addEventListener('click', function(e){
-        // e.preventDefault;
-        firstSlide.classList.toggle('hidden');
-        secondSlide.classList.toggle('hidden');
-        btnFill.classList.toggle('btn-dot-fill')
-    
-       
-    })
-})
+document.querySelectorAll(".btn-dot").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    // e.preventDefault;
+    firstSlide.classList.toggle("hidden");
+    secondSlide.classList.toggle("hidden");
+    btnFill.classList.toggle("btn-dot-fill");
+  });
+});
 // ********** //
 //    CTA
 // ********** //
 
-
-const form = document.querySelector('.form');
-const username = document.querySelector('.username');
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const btn = document.querySelector('.btn-form');
-const small = document.querySelector('.small');
-
+const form = document.querySelector(".form");
+const username = document.querySelector(".username");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const btn = document.querySelector(".btn-form");
+const small = document.querySelector(".small");
 
 //Show input error message
-const showError = function(input, message){
+const showError = function (input, message) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
   small.innerText = message;
-}
+};
 
 //Show input success outline
-const showSuccess = function(input){
+const showSuccess = function (input) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control success';
-}
+  formControl.className = "form-control success";
+};
 //Check email is valid
 function isValidEmail(email) {
   const re =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return re.test(String(email).toLowerCase())
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
 }
 
-
-
-form.addEventListener('click', function(e) {
+form.addEventListener("click", function (e) {
   e.preventDefault;
-  if (username.value === ''){
-    showError(username, 'Username is required')
-  }else{
-    showSuccess(username)
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
   }
 
-  if (email.value === ''){
-    showError(email, 'Email is required')
-  }else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid')
-  }
-  else{
-    showSuccess(email)
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
   }
 
-  if (password.value === ''){
-    showError(password, 'Password is required')
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
   }
-    else {
-    showSuccess(password)
-  }
-})
+});
 
 // MAKE MOBILE NAVIGATION WORK
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerNavEl = document.querySelector('.header');
+const btnNav = document.querySelector(".btn-mobile-nav");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-link");
 
-btnNavEl.addEventListener('click', function(){
-  headerNavEl.classList.toggle('nav-open')
-})
+// Toggle navigation when button is clicked
+btnNav.addEventListener("click", function () {
+  document.body.classList.toggle("nav-open");
+});
 
-
+// Close mobile navigation when a menu item is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    document.body.classList.remove("nav-open");
+  });
+});
 
 // footer year
 
-const yearEl = document.querySelector('.year');
+const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-console.log(currentYear)
-yearEl.textContent = currentYear
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(currentYear);
+yearEl.textContent = currentYear;
